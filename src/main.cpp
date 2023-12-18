79ec58eccbf2fa4a2a255addce027ac3057f553e
HnuAiSimOpt <seminarwh@163.com>
2023-12-18 16:33:48 +0800

    merge dev-YinJiChao

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ void main()
     // 材料路径
     // std::string path = "E:\\CADCAE_BY_ME\\model\\C3D4\\Job-1.inp";
     // std::string path = "E:/CADCAE_project/model/mix_ele_model/shaft_bracket/Job-1.inp";
-    // std::string path = "F:/OpenFEM/model/Job-1.inp";
+    std::string path = "F:/OpenFEM/model/Job-1.inp";
     // std::string path = "E:/project/CADCAE/bracket_nonconforming/inp/model.inp";//非协调路径
 
 
@@ -42,19 +42,20 @@ void main()
     cae_item.pre_info(load_set_keyword, load_value_keyword, dis_set_keyword);
 
     // 执行结构响应分析
-    string result_path = "E:\\CADCAE_BY_ME\\model\\output\\dis_C3D4.vtk";
-    string path_abaqus = "E:\\CADCAE_BY_ME\\model\\Abaqus_U_C3D4.txt";
-    cae_item.implict_analysis(result_path, path_abaqus);
+    //string result_path = "E:\\CADCAE_BY_ME\\model\\output\\dis_C3D4.vtk";
+    //string path_abaqus = "E:\\CADCAE_BY_ME\\model\\Abaqus_U_C3D4.txt";
+    //cae_item.implict_analysis(result_path, path_abaqus);
 
-    // string result_path = "F:/OpenFEM/model/";
-    // string path_abaqus = "F:/OpenFEM/model/Abaqus_U.txt";
-    // cae_item.data_cae_.time_total_ = 0.1;
-    // cae_item.data_cae_.time_step_ = 0.0;
-    // cae_item.explicit_analysis(result_path, path_abaqus);
+    // 执行结构响应分析
     // string result_path = "E:/project/CADCAE/bracket_nonconforming/output/dis_with_error.vtk";
     // string path_abaqus = "E:/project/CADCAE/bracket_nonconforming/output/Abaqus_U.txt";
     // cae_item.implict_analysis(result_path, path_abaqus);
 
+    // 执行结构响应分析
+    string result_path = "F:/OpenFEM/model/dis_with_error.vtk";
+    string path_abaqus = "F:/OpenFEM/model/Abaqus_U.txt";
+    cae_item.implict_analysis(result_path, path_abaqus);
+    // 执行动力学分析
     // string result_path = "F:/OpenFEM/model/";
     // string path_abaqus = "F:/OpenFEM/model/Abaqus_U.txt";
     // cae_item.data_cae_.time_total_ = 0.1;
